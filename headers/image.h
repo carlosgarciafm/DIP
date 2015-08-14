bfbae4c174b908d0dabd74287375395f70b00ceb
Carlos García <carlosgarciafm@gmail.com>
2015-08-13 23:46:40 -0500

    Added consulting methods.

--- headers/image.h
+++ headers/image.h
@@ -11,11 +11,13 @@ public:
   Image(int width, int height, int max);
   Image(const Image& other);
 
-  int getPixel(int j, int i) const {
-    return _pixels[(i * _width) + j];
+  int width() const { return _width; }
+  int height() const { return _height; }
+  int getPixel(int i, int j) const {
+    return _pixels[(j * _width) + i];
   }
-  int setPixel(int j, int i, int value) {
-    _pixels[(i * _width) + j] = value;
+  int setPixel(int i, int j, int value) {
+    _pixels[(j * _width) + i] = value;
   }
   void read(std::string file_name);
   void write(std::string file_name) const;
