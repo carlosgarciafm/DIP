ba11c93c47a57e4418a853cd9dbd0d3f96e2d45d
Carlos García <carlosgarciafm@gmail.com>
2015-08-16 00:21:53 -0500

    Clean up.

--- headers/image.h
+++ headers/image.h
@@ -21,7 +21,7 @@ public:
   }
   void read(std::string file_name);
   void write(std::string file_name) const;
-  Image chunk(int j1, int i1, int j2, int i2) const;     // j are  width; i are height
+  Image chunk(int j1, int i1, int j2, int i2) const;
   void stamp(int j, int i, const Image& P);
   void negative();
   void posterize(int levels);
--- source/main.cpp
+++ source/main.cpp
@@ -4,32 +4,5 @@
 using namespace std;
 
 int main() {
-  Image I;
-  // for(int i = 0; i < I.height(); i++) {
-  //   for(int j = 0; j < I.width(); j++) {
-  //     I.setPixel(j, i, j);
-  //   }
-  // }
-  I.read("pbm/bc.pgm");
-
-  // Image A;
-  // int j1 = 0, i1 = 0, j2 = 0, i2 = 0;
-  // cout << "Please input the coordinates of the start and end points, use (j1,i1) and (j2,i2): ";
-  // cin >> j1 >> i1 >> j2 >> i2;
-  // A = I.chunk(j1, i1, j2, i2);
-  //
-  // Image P(800, 800, 255);
-  // int j3 = 0, i3 = 0;
-  // cout << "Please input the coordinates where you want to stamp the chunk, use (j3, i3): ";
-  // cin >> j3 >> i3;
-  // P.stamp(j3, i3, A);
-  // P.write("pgm/output_stamp.pgm");
-  // P.negative();
-  // P.write("pgm/output_negative.pgm");
-
-  int levels = 0;
-  cin >> levels;
-  I.posterize(levels);
-  I.write("pbm/output_posterize.pgm");
-
+  return 0;
 }
