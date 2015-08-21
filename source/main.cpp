290c167c2904eb8763afd83e17798cec7ae34d71
Carlos García <carlosgarciafm@gmail.com>
2015-08-20 23:20:06 -0500

    Clean up.

--- source/main.cpp
+++ source/main.cpp
@@ -4,13 +4,5 @@
 using namespace std;
 
 int main() {
-  Image I;
-  I.read("pbm/bc.pgm");
-  // for(int i = 0; i < I.height(); i++) {
-  //   for(int j = 0; j < I.width(); j++) {
-  //     if(j < i) I.setPixel(j, i, I.max());
-  //   }
-  // }
-  I.histogram("pbm/plot.pgm");
   return 0;
 }
